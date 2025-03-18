2824b4d5667ae5c995e9f88c77e73a56767e9d8a
ram <maheshwariiram@gmail.com>
2025-03-19 00:11:24 +0100

    fix: package directory

--- src/main/kotlin/com/library/system/ServiceApplication.kt
+++ src/main/kotlin/com/library/system/ServiceApplication.kt
@@ -1,4 +1,4 @@
-package com.lms.service
+package com.library.system
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/test/kotlin/com/library/system/ServiceApplicationTests.kt
+++ src/test/kotlin/com/library/system/ServiceApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.lms.service
+package com.library.system
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
