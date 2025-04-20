e19e584731c75d3bd330d9e14585bb5c2e9f78a0
ram <maheshwariiram@gmail.com>
2025-04-20 21:11:14 +0200

    fix: fixed the "The following declarations have the same JVM signature" error

--- src/main/kotlin/com/library/system/model/Book.kt
+++ src/main/kotlin/com/library/system/model/Book.kt
@@ -2,6 +2,6 @@ package com.library.system.model
 
 import java.util.UUID
 
-data class Book(val id: UUID, var title: String, val author: String, var available: Boolean) {
+data class Book(val id: UUID, var title: String, val author: String, private var available: Boolean) {
     var isAvailable: Boolean = false
 }
