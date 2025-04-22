7f62b5663d676ecbd023dba32d8e27a175615a1e
ram <maheshwariiram@gmail.com>
2025-04-22 20:45:58 +0200

    feat: web

--- src/main/kotlin/com/library/system/web/BookController.kt
+++ src/main/kotlin/com/library/system/web/BookController.kt
@@ -1,4 +1,55 @@
 package com.library.system.web
 
-class BookController {
+import com.library.system.model.Book
+import com.library.system.services.BookService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.util.UUID
+
+@RestController
+@RequestMapping("/books")
+class BookController(private val bookService: BookService) {
+
+    @PostMapping
+    fun addBook(@RequestBody book: Book): ResponseEntity<Book> {
+        val newBook = bookService.addBook(book)
+        return ResponseEntity(newBook, HttpStatus.CREATED)
+    }
+
+    @GetMapping("/{id}")
+    fun getBookById(@PathVariable id: UUID): ResponseEntity<Book> {
+        return try {
+            val book = bookService.getBookById(id)
+            ResponseEntity.ok(book)
+        } catch (e: Exception) {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @GetMapping
+    fun getAllBooks(): ResponseEntity<List<Book>> {
+        val books = bookService.getAllBooks()
+        return ResponseEntity.ok(books)
+    }
+
+    @PutMapping("/{id}")
+    fun updateBook(@PathVariable id: UUID, @RequestBody updatedBook: Book): ResponseEntity<Book> {
+        return try {
+            val book = bookService.updateBook(id, updatedBook)
+            ResponseEntity.ok(book)
+        } catch (e: Exception) {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteBook(@PathVariable id: UUID): ResponseEntity<Void> {
+        return try {
+            bookService.deleteBookById(id)
+            ResponseEntity.noContent().build()
+        } catch (e: Exception) {
+            ResponseEntity.notFound().build()
+        }
+    }
 }
--- src/main/kotlin/com/library/system/web/UserController.kt
+++ src/main/kotlin/com/library/system/web/UserController.kt
@@ -1,4 +1,21 @@
 package com.library.system.web
 
-class UserController {
+import com.library.system.model.User
+import com.library.system.services.UserService
+import org.springframework.web.bind.annotation.*
+import java.util.UUID
+
+@RestController
+@RequestMapping("/users")
+class UserController(private val userService: UserService) {
+
+    @PostMapping
+    fun registerUser(@RequestBody user: User): User = userService.registerUser(user)
+
+    @GetMapping("/{id}")
+    fun getUserById(@PathVariable id: UUID): User = userService.getUserById(id)
+
+    @PutMapping("/{id}")
+    fun updateUser(@PathVariable id: UUID, @RequestBody user: User): User =
+        userService.updateUser(id, user)
 }
